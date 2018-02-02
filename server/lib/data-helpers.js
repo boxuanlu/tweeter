1e6a22af32caba0b367ac66ad0ea98833911fcd4
boxuanlu <boxuan@ualberta.ca>
2018-02-01 22:00:28 -0700

    new change

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -13,7 +13,6 @@ module.exports = function makeDataHelpers(db) {
     getTweets: function(callback) {
       db.collection("tweets").find().toArray((err, results) => {
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        console.log(results);
         callback(null, results.sort(sortNewestFirst));
       });
     }
